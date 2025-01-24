a7ab4ec932321d7ba49228f8231752f79400e8a8
Yarley Tique <arley.skn@gmail.com>
2025-01-24 20:43:29 +0000

    carta generada

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Chau prettier!!");
-};
+function generarCarta() {
+  let palos = ['♥', '♠', '♣', '♦'];
+  let valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+
+  let palo = palos[Math.floor(Math.random() * palos.length)];
+  let valor = valores[Math.floor(Math.random() * valores.length)];
+
+
+  let carta = document.getElementById('carta');
+  let colorClase = (palo === '♥' || palo === '♠') ? 'rojo' : 'negro';
+  carta.innerHTML = `
+    <div class="palo palo-top-left ${colorClase}">${palo}</div>
+    <div class="valor">${valor}</div>
+    <div class="palo palo-bottom-right ${colorClase}">${palo}</div>
+  `;
+}
+
+
+window.onload = generarCarta;
